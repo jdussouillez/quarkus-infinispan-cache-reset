8f2dd5f01f34502ecc1ef44d2910532f814fa824
Junior DUSSOUILLEZ <junior.dussouillez@gmail.com>
2024-02-21 10:49:57 +0100

    feat: add a cache recovery service

---
+++ src/main/java/com/github/jdussouillez/CacheRecoveryService.java
@@ -0,0 +1,99 @@
+package com.github.jdussouillez;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import java.util.List;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.infinispan.client.hotrod.RemoteCache;
+import org.infinispan.client.hotrod.RemoteCacheManager;
+import org.infinispan.client.hotrod.configuration.ConfigurationBuilder;
+import org.infinispan.protostream.GeneratedSchema;
+import org.infinispan.query.remote.client.ProtobufMetadataManagerConstants;
+
+/**
+ * Cache recovery service
+ *
+ * <p>
+ * If a "cache not found" error is detected it means the Infinispan server died and another instance spawn.
+ * This service will create the caches and register the schemas at runtime so {@link CacheService} can still use caches.
+ * </p>
+ */
+@ApplicationScoped
+public class CacheRecoveryService {
+
+    private static final List<? extends GeneratedSchema> SCHEMAS = List.of(
+        // OPTIMIZE: detect schema classes automatically using reflection?
+        new MathCacheSchemaImpl()
+    );
+
+    @Inject
+    protected RemoteCacheManager cacheManager;
+
+    @ConfigProperty(name = "myapp.infinispan.auto-recovery", defaultValue = "true")
+    protected boolean autoRecovery;
+
+    @ConfigProperty(name = "quarkus.infinispan-client.username")
+    protected String username;
+
+    @ConfigProperty(name = "quarkus.infinispan-client.password")
+    protected String password;
+
+    public void recover(final Throwable thrw) {
+        if (!needsRecovery(thrw)) {
+            return;
+        }
+        System.out.println("Cache recovery in progress...");
+        var recoveryCacheManager = duplicateCacheManager();
+        try {
+            // Register schemas
+            RemoteCache<String, String> metadataCache = recoveryCacheManager
+                .getCache(ProtobufMetadataManagerConstants.PROTOBUF_METADATA_CACHE_NAME);
+            SCHEMAS.forEach(schema -> metadataCache.put(schema.getProtoFileName(), schema.getProtoFile()));
+            System.out.println("Schemas registered");
+
+            // Get the caches once to create them
+            recoveryCacheManager.getConfiguration().remoteCaches()
+                .keySet()
+                .stream()
+                .forEach(recoveryCacheManager::getCache);
+            System.out.println("Cache recovery completed");
+        } catch (RuntimeException ex) {
+            System.err.println("Error when recovering Infinispan: " + ex.getMessage());
+        } finally {
+            recoveryCacheManager.stop();
+        }
+    }
+
+    private boolean needsRecovery(final Throwable thrw) {
+        if (!autoRecovery) {
+            return false;
+        }
+        // Dirty hack because CacheNotFoundException is not public
+        return thrw.getMessage().contains("org.infinispan.server.hotrod.CacheNotFoundException");
+    }
+
+    private RemoteCacheManager duplicateCacheManager() {
+        var originalConf = cacheManager.getConfiguration();
+        var confBuilder = new ConfigurationBuilder()
+            .clientIntelligence(originalConf.clientIntelligence());
+        // Servers
+        originalConf.servers().forEach(server ->
+            confBuilder.addServer()
+                .host(server.host())
+                .port(server.port())
+        );
+        // Security
+        confBuilder
+            .security()
+            .authentication()
+            .username(username)
+            .password(password);
+        // Caches
+        originalConf.remoteCaches().values().forEach(cache ->
+            confBuilder
+                .remoteCache(cache.name())
+                .configuration(cache.configuration())
+        );
+        return new RemoteCacheManager(confBuilder.build());
+    }
+}
--- src/main/java/com/github/jdussouillez/CacheService.java
+++ src/main/java/com/github/jdussouillez/CacheService.java
@@ -5,23 +5,21 @@ import io.smallrye.mutiny.Uni;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import java.time.Duration;
-import java.util.List;
 import java.util.Optional;
 import java.util.function.Function;
 import org.infinispan.client.hotrod.RemoteCache;
 import org.infinispan.client.hotrod.RemoteCacheManager;
-import org.infinispan.protostream.GeneratedSchema;
-import org.infinispan.query.remote.client.ProtobufMetadataManagerConstants;
 
 @ApplicationScoped
 public class CacheService {
 
     private static final int REQUEST_TIMEOUT = 3_000;
 
-    private static final boolean ENABLE_AUTO_RECOVERY = false; // TODO: change this to enable auto recovery
-
     @Inject
     protected RemoteCacheManager cacheManager;
+
+    @Inject
+    protected CacheRecoveryService cacheRecoveryService;
 
     public <K, V> Uni<V> getOrFetch(final RemoteCache<K, V> cache, final K key, final Function<K, Uni<V>> fetch) {
         return get(cache, key)
@@ -45,7 +43,7 @@ public class CacheService {
             .onFailure()
             .invoke(ex -> {
                 System.err.println("Failed to read in cache: " + ex.getMessage());
-                tryRecovery(ex);
+                recover(ex);
             })
             .onFailure()
             .recoverWithItem(Optional.empty());
@@ -60,41 +58,13 @@ public class CacheService {
             .onFailure()
             .invoke(ex -> {
                 System.err.println("Failed to write in cache: " + ex.getMessage());
-                tryRecovery(ex);
+                recover(ex);
             })
             .onFailure()
             .recoverWithItem(value);
     }
 
-    private <K, V> void tryRecovery(final Throwable cause) {
-        if (!ENABLE_AUTO_RECOVERY) {
-            return;
-        }
-        // TODO: fix this. Dirty hack because CacheNotFoundException is not public
-        if (!cause.getMessage().contains("org.infinispan.server.hotrod.CacheNotFoundException")) {
-            return;
-        }
-        System.out.println("Try to recover caches/schemas...");
-        // Register schemas
-        List<? extends GeneratedSchema> schemas = List.of(
-            new MathCacheSchemaImpl()
-        );
-        RemoteCache<String, String> protoMetadataCache = cacheManager
-            .getCache(ProtobufMetadataManagerConstants.PROTOBUF_METADATA_CACHE_NAME);
-        for (var schema : schemas) {
-            protoMetadataCache.put(schema.getProtoFileName(), schema.getProtoFile());
-        }
-        var errors = protoMetadataCache.get(ProtobufMetadataManagerConstants.ERRORS_KEY_SUFFIX);
-        if (errors != null) {
-            System.err.println("Invalid proto schema: " + errors);
-            return;
-        }
-        System.out.println("Schema registered");
-        // Create caches
-        cacheManager.getConfiguration().remoteCaches()
-            .values()
-            .stream()
-            .forEach(cache -> cacheManager.administration().createCache(cache.name(), name -> cache.configuration()));
-        System.out.println("Caches created. End of recovery");
+    private void recover(final Throwable cause) {
+        cacheRecoveryService.recover(cause);
     }
 }
